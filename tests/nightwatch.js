436310bb8c98a5edc8daaddbceed3f6906b8bc4a
Daniel Bispo <daniel.bispo@bemobi.com.br>
2017-08-09 14:32:41 -0300

    Arrumados os testes e ajustes do nightwatch

--- tests/nightwatch.js
+++ tests/nightwatch.js
@@ -1,19 +1,13 @@
-require('nightwatch/bin/runner.js');
-
 module.exports = {
-  'step one' : function (browser) {
+  'Demo test Google' : function (browser) {
     browser
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
       .setValue('input[type=text]', 'nightwatch')
-      .waitForElementVisible('button[name=btnG]', 1000)
-  },
-
-  'step two' : function (browser) {
-    browser
-      .click('button[name=btnG]')
+      .waitForElementVisible('input[name=btnK]', 1000)
+      .click('input[name=btnK]')
       .pause(1000)
       .assert.containsText('#main', 'Night Watch')
       .end();
   }
-}
+};
